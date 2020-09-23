219d4d9e0533c97240a8db14baa83aa9951c4bcb
DESKTOP-O8BLG7E\admin <tuksin_lekrut@cmu.ac.th>
2020-09-24 01:36:41 +0700

    Ver.2

---
+++ Lab_game_09/Assets/Code thislab/GameplayMenuControlScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameplayMenuControlScript : MonoBehaviour
+{
+    [SerializeField] Button _backButton;
+    [SerializeField] Button _nextButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _backButton.onClick.AddListener(delegate { BackToMainMenuButtonClick(_backButton); });
+        _nextButton.onClick.AddListener(delegate { NextToGameplayButtonClick(_nextButton); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void BackToMainMenuButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneGameplay");
+        SceneManager.LoadScene("SceneMainMenu");
+    }
+
+    public void NextToGameplayButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneGameplay");
+        SceneManager.LoadScene("SceneGameplay2");
+    }
+}
---
+++ Lab_game_09/Assets/Code thislab/SingletonGameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SingletonGameManager : MonoBehaviour
+{
+   
+    void Start()
+    {
+        GetComponent<Text>().text = GameApplicationManager.Instance.DIFFICULTY_LEVEL_NAMES[GameApplicationManager.Instance.DifficultyLevel];
+    }
+
+    
+}
